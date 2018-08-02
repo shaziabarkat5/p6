f8a0b9533f64b0aae8dba480a1dec1526d695091
shaziabarkat5 <38329467+shaziabarkat5@users.noreply.github.com>
2018-08-02 23:32:41 +0530

    Add files via upload

---
+++ powder.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.Math;
+
+public class powder {
+
+	public static void main(String[] args) {
+		double a1,a2;
+		Scanner a = new Scanner(System.in);
+		a1= a.nextDouble();
+		a2 = a.nextDouble();
+		a.close();
+		System.out.println(Math.pow(a1, a2));
+
+	}
+
+}
